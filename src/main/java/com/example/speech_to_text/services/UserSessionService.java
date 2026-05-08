4fb8592a742ac60d423fd6627104a3deddd16e7a
chilan204 <lanno442@gmail.com>
2026-05-08 19:41:50 +0700

    Add conflict command by role

--- src/main/java/com/example/speech_to_text/services/UserSessionService.java
+++ src/main/java/com/example/speech_to_text/services/UserSessionService.java
@@ -3,7 +3,6 @@ package com.example.speech_to_text.services;
 import com.example.speech_to_text.dto.response.UserSessionResponse;
 import com.example.speech_to_text.dto.response.VoiceCommandResponse;
 import com.example.speech_to_text.entities.User;
-import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.List;
 
